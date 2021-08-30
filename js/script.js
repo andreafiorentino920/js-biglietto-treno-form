a7ec4c8de952beb745f15fda58cdcdbff36973f6
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-08-30 20:30:59 +0100

    Push

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Selezione bottoni 
+var btnGenera = document.getElementById("genera");
+var btnAnnulla = document.getElementById("annulla");
+
+// Click bottone
+btnGenera.addEventListener("click",
+    function(){
+
+    }
+)
+
+//Acquisizione valori campi input e select 
+var nome = document.getElementById("nome").value;
+var km = parseInt(document.getElementById("km").value);
+var fasciaEta = document.getElementById("eta").value;
+
+// Calcolo il prezzo in base alla fascia d'età
+
+
+// Stampo dati nel div biglietto
+// Inserire nome del passeggero
+// Inserire il tipo di offerta
+// Inserire codice cp e carrozza
