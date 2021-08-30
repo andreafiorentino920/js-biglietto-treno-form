75ffb567f2255396d7d24911ea116a3d45e7b062
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-08-30 20:54:51 +0100

    Push 3

--- js/script.js
+++ js/script.js
@@ -5,9 +5,12 @@ var btnAnnulla = document.getElementById("annulla");
 // Click bottone
 btnGenera.addEventListener("click",
     function(){
-
+        // controllo se il box ha la classe open la rimuovo
+        if (biglietto.classLIst.contains("open")){
+            biglietto.classLIst.remove("open")     
+        } else 
+        biglietto.classlist.add("open")
     }
-)
 
 //Acquisizione valori campi input e select 
 var nome = document.getElementById("nome").value;
@@ -19,5 +22,17 @@ var fasciaEta = document.getElementById("eta").value;
 
 // Stampo dati nel div biglietto
 // Inserire nome del passeggero
+document.getElementById("nome-passeggero") = document.write("nome")
 // Inserire il tipo di offerta
 // Inserire codice cp e carrozza
+
+// Mostro il biglietto
+document.getElementById("biglietto").classList.add("open")
+
+// al click del bottone annulla 
+btnAnnulla.addEventListener("click",
+    function(){
+        // nascondo il biglietto
+        document.getElementById("biglietto").classList.remove("open")
+    }
+)
