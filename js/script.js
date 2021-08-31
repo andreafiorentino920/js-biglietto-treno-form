204ea91162e31ed089f4b2d595a7d510fe432362
andreafiorentino920 <andreafiorentino920@gmail.com>
2021-08-31 10:48:59 +0100

    Esercizio completo con correzione classe open

--- js/script.js
+++ js/script.js
@@ -5,34 +5,68 @@ var btnAnnulla = document.getElementById("annulla");
 // Click bottone
 btnGenera.addEventListener("click",
     function(){
-        // controllo se il box ha la classe open la rimuovo
-        if (biglietto.classLIst.contains("open")){
-            biglietto.classLIst.remove("open")     
-        } else 
-        biglietto.classlist.add("open")
+      var biglietto = document.getElementById("biglietto");
+        // controllo se il box ha la classe open
+      biglietto.classList.add("open");
+      var nome = document.getElementById("nome").value;
+      var km = document.getElementById("km").value;
+      var fasciaEta = document.getElementById("fascia").value;
+      if (nome != "" && !isNaN(km) && km > 0 && fasciaEta != ""){
+        // Calcolo il prezzo in base alla fascia d'età
+        var prezzo = km * 0.21;
+        var tipoOfferta = "Biglietto Standard";
+        var discount20 = prezzo * 0.20;
+        var discount40 = prezzo * 0.40;
+        if (fasciaEta == "minorenne"){
+            prezzo = prezzo - discount20;
+            tipoOfferta = "Sconto Minorenne";
+        }else if(fasciaEta == "over"){
+            prezzo = prezzo - discount40;
+            tipoOfferta = "Sconto Silver"
+        }    
+        // Arrotondo per i primi 2 decimali
+        prezzo = prezzo.toFixed(2);
+        // Stampo dati nel div biglietto
+        document.getElementById("nome-passeggero").innerHTML = nome;
+        document.getElementById("tipo-offerta").innerHTML = tipoOfferta;
+        document.getElementById("carrozza").innerHTML = Math.floor(Math.random()*9)+1;
+        document.getElementById("codice").innerHTML = Math.floor(Math.random()* (99999 - 90000))+90000;
+        document.getElementById("totale").innerHTML = prezzo + "€";
+        document.getElementById("biglietto").classList.add("open");
+      }else{
+        alert("Errore");
+      }
     }
-
-//Acquisizione valori campi input e select 
-var nome = document.getElementById("nome").value;
-var km = parseInt(document.getElementById("km").value);
-var fasciaEta = document.getElementById("eta").value;
-
-// Calcolo il prezzo in base alla fascia d'età
-
-
-// Stampo dati nel div biglietto
-// Inserire nome del passeggero
-document.getElementById("nome-passeggero") = document.write("nome")
-// Inserire il tipo di offerta
-// Inserire codice cp e carrozza
-
-// Mostro il biglietto
-document.getElementById("biglietto").classList.add("open")
+);
 
 // al click del bottone annulla 
 btnAnnulla.addEventListener("click",
     function(){
         // nascondo il biglietto
-        document.getElementById("biglietto").classList.remove("open")
+        document.getElementById("biglietto").classList.remove("open");
+        // reset campi input 
+        document.getElementById("nome").value = "";
+        document.getElementById("km").value = "";
+        document.getElementById("fascia").selectIndex = "0";
     }
-)
+);
+
+
+
+
+
+
+
+
+
+// btnGenera.addEventListener("click",
+//     function(){
+//         var nome = document.getElementById("nome").value;
+//         var km = parseInt(document.getElementById("km").value);
+//         var carrozza = 
+//         // document.getElementById("nome-passeggero").innerHTML = nome;
+//         // document.getElementById("carrozza").innerHTML = km;
+//         // document.getElementById("codice").innerHTML = codice;
+//         // var fasciaEta = document.getElementById("eta").value;
+//     }
+// );
